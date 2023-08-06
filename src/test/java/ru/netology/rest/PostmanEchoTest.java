4e41582debf9bcb9ce184b6d99989c7cae454cc6
KatyaQA <ekaterina.rus91@mail.ru>
2023-08-06 11:17:57 +0300

    init commit

---
+++ src/test/java/ru/netology/rest/PostmanEchoTest.java
@@ -0,0 +1,25 @@
+package ru.netology.rest;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanEchoTest {
+
+    @Test
+    void returnSendData() {
+
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("hi")
+
+                .when()
+                .post("/post")
+
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("hellow" ));
+    }
+
+}
